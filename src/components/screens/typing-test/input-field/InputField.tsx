3abaa677195aa97caef03cf0e457775ed5e6a45d
Appazov <anvar978@yandex.ru>
2022-03-17 01:12:43 +0400

    Add keys checking

--- src/components/screens/typing-test/input-field/InputField.tsx
+++ src/components/screens/typing-test/input-field/InputField.tsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { KeyboardEvent, useEffect, useRef, useState } from 'react'
 import { Paper, TextField } from '@mui/material'
 import { useAppDispatch, useAppSelector } from 'hooks/redux'
 import { calcResult, setIsShowResult, setText } from 'store/reducers/TypingTestSlice'
 
 const InputField = () => {
   const [value, setValue] = useState('')
+  const [textTemplateSplitted, setTextTemplateSplitted] = useState<Array<string>>([])
+  const [letterIndex, setLetterIndex] = useState(0)
   const focusInput = useRef<HTMLTextAreaElement>()
-  const { isTestStarted, isTestFinished } = useAppSelector((state) => state.TypingTestReducer)
+  const { isTestStarted, isTestFinished, textTemplate } = useAppSelector(
+    (state) => state.TypingTestReducer
+  )
   const dispatch = useAppDispatch()
 
+  // when the test is over
   useEffect(() => {
     if (isTestFinished) {
       dispatch(setText(value))
@@ -17,12 +22,28 @@ const InputField = () => {
     }
   }, [value, isTestFinished, dispatch])
 
+  // when the test started
   useEffect(() => {
     if (isTestStarted) {
-      setValue('')
       focusInput.current?.focus()
     }
   }, [focusInput, isTestStarted])
+
+  // set separated text template for controlling input
+  useEffect(() => {
+    setTextTemplateSplitted(textTemplate.value.split(''))
+  }, [textTemplate])
+
+  const onKeyPressHandler = (event: KeyboardEvent<HTMLDivElement>, index: number) => {
+    if (event.key !== textTemplateSplitted[index]) {
+      event.preventDefault()
+      return false
+    }
+    setLetterIndex(index + 1)
+  }
+  const onKeyDownHandler = (event: KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Backspace' && letterIndex > 0) setLetterIndex(letterIndex - 1)
+  }
 
   return (
     <Paper elevation={4}>
@@ -31,6 +52,10 @@ const InputField = () => {
         onChange={(event) => {
           setValue(event.target.value)
         }}
+        onKeyPress={(event) => {
+          onKeyPressHandler(event, letterIndex)
+        }}
+        onKeyDown={onKeyDownHandler}
         inputRef={focusInput}
         multiline
         rows={5}
